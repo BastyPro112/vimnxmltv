73eb40589e3b0702b1946c1d4cb4ec6c0804eaae
BastyPro112 <157334602+BastyPro112@users.noreply.github.com>
2025-12-20 17:54:23 -0300

    channel list

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { XMLBuilder } = require("fast-xml-parser"); 
+const { XMLParser, XMLBuilder } = require("fast-xml-parser"); 
 const moment = require("moment"); 
 const axios = require("axios"); 
-const fs = require("node:fs"); 
+const fs = require("fs"); 
 const channels = require("./channels.json"); 
 const builder = new XMLBuilder({
     ignoreAttributes: false, 
@@ -9,68 +9,34 @@ const builder = new XMLBuilder({
     parseAttributeValue: true, 
     format: true 
 }); 
-let channelList = {
-    "?xml": {
-        "@_version": "1.0" 
-    }, 
-    tv: {
-        channel: [], 
-        programme: []
-    }
-}; 
-
-let channelEPGLinkList = {}; 
-
-channels.forEach(channel => {
-    channelList.tv.channel.push({
-            "display-name": channel.name,
-            "icon": {
-                "@_src": channel.logo
-            },
-            "@_id": channel.id 
-    }); 
-    channelEPGLinkList[channel.id] = channel.epg; 
-    console.log(`Canal ${channel.name} con id ${channel.id} encontrado.`)
+const parser = new XMLParser({
+    ignoreAttributes: false, 
+    attributeNamePrefix: "@_", 
+    parseAttributeValue: true, 
+    format: true 
 }); 
 
-channelList.tv.channel.forEach(async channel => {   
-    const id = channel["@_id"]; 
-    console.log("Generando para " + id); 
-    let epgs = []; 
-    try {
-        const epgreq = await axios.get(channelEPGLinkList[id], {
-             "headers": {
-                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36",
-                 "Sec-Fetch-Site": "same-origin"}}); 
-        if (!epgreq.status == 200) {
-            console.log(`Error al generar ${id}: ${epgreq.status}`)
-            return; 
-        } 
-        const epgJson = epgreq.data; 
-        let epg; 
-        epgJson.programacion.forEach(program => {
-            channelList.tv.programme.push({
-                "title": program["titulo"], 
-                "desc": program["subtitulo"], 
-                "@_start": moment(program["inicio"]).format("YYYYMMDDhhmmss ZZ"), 
-                "@_stop": moment(program["fin"]).format("YYYYMMDDhhmmss ZZ"), 
-            })
-        }); 
+const afterDays = 5; 
+const beforeDays = -2; 
+let datesXMLArray = []; 
+
+for(let i = beforeDays; i <= afterDays; i++) {
+    datesXMLArray.push(moment().add(i, "days").format("YYYYMMDD") + ".xml"); 
+}
+
+console.log(datesXMLArray); 
+
+channels.forEach(async channel => {
+    for(let fi = beforeDays; i <= afterDays; i++) {
+        let i = fi + 2; 
+        let epgPath = channel.epg + datesXMLArray[i]; 
+        console.log("Intentando hacer una solicitud a " + epgPath); 
         try {
-            let epg = builder.build(channelList); 
-            console.log("Intentando escribir a la ruta establecida")
-            fs.writeFileSync('./epg.xml', epg); 
-            console.log("Escrito exitosamente")
-        } catch (err) {
-            console.error("Error al escribir: " + err);
+            const epgXml = await axios.get(epgPath) 
+            const epgJson = parser.parse(epgXml); 
+            
+        } catch(e) {
+
         }
-    } catch(e) { 
-        console.error("Error al generar: " + e); 
-        return; 
     }
 })
-
-
-
-console.log(JSON.stringify(channelList)); 
-// console.log(builder.build(channelList)) 
---
+++ test2.js
@@ -0,0 +1,24 @@
+const axios = require("axios"); 
+const { XMLParser } = require("fast-xml-parser"); 
+const parser = new XMLParser({
+     ignoreAttributes: false, // Set to false to parse attributes
+  attributeNamePrefix: "@_", // Add a prefix to attribute names
+  parseAttributeValue: true,
+})
+axios.get("https://epg.vimn.com/nickelodeon_north/xmltvlegal/las/20260131.xml")
+.then(resp => {
+    console.log(JSON.stringify(parser.parse(resp.data)))
+}); 
+
+
+
+
+
+
+
+
+
+
+
+
+
