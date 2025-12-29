7ef7f018c34d1744aeefe8847a954ab5040306d7
BastyPro112 <157334602+BastyPro112@users.noreply.github.com>
2025-12-29 13:09:32 -0300

    f

--- test2.js
+++ test2.js
@@ -1,13 +1,17 @@
 const axios = require("axios"); 
+const fs = require("fs")
 const { XMLParser } = require("fast-xml-parser"); 
 const parser = new XMLParser({
      ignoreAttributes: false, // Set to false to parse attributes
   attributeNamePrefix: "@_", // Add a prefix to attribute names
-  parseAttributeValue: true,
+  parseAttributeValue: true, 
+  format: true 
 })
 axios.get("https://epg.vimn.com/nickelodeon_north/xmltvlegal/las/20260131.xml")
 .then(resp => {
-    console.log(JSON.stringify(parser.parse(resp.data)))
+    let d = JSON.stringify(parser.parse(resp.data)) 
+    //console.log(); 
+    fs.writeFileSync("./outtest.json", d)
 }); 
 
 
